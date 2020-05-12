e3e57d37c2950f67b8eb22f19f76686a61e538c6
Ilyin Sergei <steelseriesisv@gmail.com>
2020-05-12 18:36:25 +0300

    initial

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.use("/api/users", require("./routes/users.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 const PORT = process.env.PORT || config.get("port") || 5000;
